48ee19d30367f55fc3f375c3d400ecd6e5ed1ac3
Kyle King <highergroundstudio@users.noreply.github.com>
2020-06-16 17:14:57 -0600

    Cleanup

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const geolib = require("geolib");
 const Combinatorics = require("js-combinatorics");
-// const jsonfile = require("jsonfile");
-// var _ = require("lodash");
-
-const fs = require('fs')
 
 let pointDistances = [];
 let points = [];
@@ -15,21 +11,23 @@ const params = {
   maxDistanceBetween: 105, //meters
 };
 
-const json = require('./points.json')
+const json = require("./points.json");
 
 json.features.map(function (a) {
   points[a.properties.title] = a.geometry.coordinates;
 });
-console.dir(points)
 
 // Get all possible combinations of points to compare
 Combinatorics.combination(Object.keys(points), 2).map(function (a) {
   let distance = geolib.getDistance(points[a[0]], points[a[1]]);
 
   //Only add point distances that are between our params
-  //   if (distance > params.minDistanceBetween && distance < params.maxDistanceBetween) {
-  pointDistances[a] = distance;
-  //   }
+  if (
+    distance > params.minDistanceBetween &&
+    distance < params.maxDistanceBetween
+  ) {
+    pointDistances[a] = distance;
+  }
 });
 
 console.log(pointDistances);
